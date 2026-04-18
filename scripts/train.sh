ff4d930a73f24b84fb33fe51ae0b1361738c7d9d
Wahib Kapdi <wahib@metaborong.com>
2026-04-17 23:35:40 -0500

    train.sh Fixed

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")/.."
 
 sbatch ./scripts/slurm/cmshc_mirflickr25k_64bit.sbatch
 sbatch ./scripts/slurm/cmshc_mirflickr25k_128bit_nocm.sbatch
